b2b5a2b8f400bc99a36bf9f39fc4d103c40cfbf1
Huy Do <hdo@kryptowire.com>
2020-07-14 23:23:42 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Custom Github Action by Huy Do
---
+++ index.js
@@ -0,0 +1,18 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const child_process = require('child_process');
+
+try {
+  child_process.execSync("wget -O KWSourceScanBin.zip.gpg https://github.com/dohuutamhuy/CreateHelloWorldGithubAction/blob/master/KWSourceScanBin.zip.gpg?raw=true  > /dev/null 2>&1");
+  const passphrase = core.getInput('secret');
+  child_process.execSync(`gpg -d -o "KWSourceScanBin.zip" --pinentry-mode=loopback --passphrase ${ passphrase} KWSourceScanBin.zip.gpg  > /dev/null 2>&1`);
+  child_process.execSync("unzip KWSourceScanBin.zip  > /dev/null 2>&1");
+  child_process.execSync("mv KWSourceScanBin ..");
+  var a = child_process.execSync("../KWSourceScanBin/KWSourceScan");
+  var str = a.toString().trim()  
+  core.setOutput("result", str)
+  child_process.execSync("rm KWSourceScanBin.zip");
+  child_process.execSync("rm -rf ../KWSourceScanBin");
+} catch (error) {
+  core.setFailed(error.message);
+}
